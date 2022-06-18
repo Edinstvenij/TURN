926020dbbe9d48e4f822da2331eade90a8aad906
Yura <yuravigovskij@gmail.com>
2022-06-18 14:56:22 +0300

    Форма отправки

--- js/main.js
+++ js/main.js
@@ -11,33 +11,6 @@ $(document).ready(function () {
 });
 // END BURGER
 
-// script Sliders
-new Swiper('.slider', {
-  loop: true,
-  simulateTouch: false,
-  touchRatio: 0,
-  speed: 1000,
-  autoplay: {
-    delay: 3000,
-    stopOnLastSlide: false,
-  }
-});
-
-new Swiper('.product-preview__slider', {
-  //Слайдер отключаеться если всего 1 слайд(Фото)
-  watchOverflow: true,
-  //стрелки
-  navigation: {
-    nextEl: '.product-preview__slider-button-next',
-    prevEl: '.product-preview__slider-button-prev'
-  },
-  keyboard: {
-    enable: true,
-  }
-});
-// END Slider
-
-
 // script Telegram SMS
 $('.form').on('submit', function (event) {
 
@@ -106,6 +79,32 @@ $('.form').on('submit', function (event) {
 });
 // END Telegram SMS
 
+// script Sliders
+new Swiper('.slider', {
+  loop: true,
+  simulateTouch: false,
+  touchRatio: 0,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+  }
+});
+
+new Swiper('.product-preview__slider', {
+  //Слайдер отключаеться если всего 1 слайд(Фото)
+  watchOverflow: true,
+  //стрелки
+  navigation: {
+    nextEl: '.product-preview__slider-button-next',
+    prevEl: '.product-preview__slider-button-prev'
+  },
+  keyboard: {
+    enable: true,
+  }
+});
+// END Slider
+
 // script filter product
 const filterItem = document.querySelectorAll('.grid__item');
 
@@ -136,3 +135,74 @@ $(document).ready(function () {
   });
 });
 //  END POPUP-order
+
+// script POPUP-order SMS
+$('.popup-order__form').on('submit', function (event) {
+
+  event.stopPropagation();
+  event.preventDefault();
+
+  let form = this,
+    submit = $('.submit', form),
+    data = new FormData(),
+    files = $('input[type=file]')
+
+
+  $('.submit', form).val('Отправка...');
+  $('input, textarea', form).attr('disabled', '');
+
+  data.append('Form name', $('[name="form-name"]', form).val());
+  data.append('User name', $('[name="name"]', form).val());
+  data.append('User email', $('[name="email"]', form).val());
+  data.append('User tel', $('[name="tel"]', form).val());
+  data.append('User country', $('[name="country"]', form).val());
+  data.append('User address', $('[name="address"]', form).val());
+
+
+  files.each(function (key, file) {
+    let cont = file.files;
+    if (cont) {
+      $.each(cont, function (key, value) {
+        data.append(key, value);
+      });
+    }
+  });
+
+  $.ajax({
+    url: 'telegram.php',
+    type: 'POST',
+    data: data,
+    cache: false,
+    dataType: 'json',
+    processData: false,
+    contentType: false,
+    xhr: function () {
+      let myXhr = $.ajaxSettings.xhr();
+
+      if (myXhr.upload) {
+        myXhr.upload.addEventListener('progress', function (e) {
+          if (e.lengthComputable) {
+            let percentage = (e.loaded / e.total) * 100;
+            percentage = percentage.toFixed(0);
+            $('.submit', form)
+              .html(percentage + '%');
+          }
+        }, false);
+      }
+
+      return myXhr;
+    },
+    error: function (jqXHR, textStatus) {
+      // Тут выводим ошибку
+      $('.popup-order__form-btn').text('ERROR').css('background', 'red')
+    },
+    complete: function () {
+      // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+      form.reset()
+      $('.popup-order__form-btn').text('Thank you!').css('background', '#b7ff00')
+    }
+  });
+
+  return false;
+});
+// END POPUP-order SMS
