008add4444853efd401d42c9c52d6c3ee7e0522e
Yura <yuravigovskij@gmail.com>
2022-07-06 20:07:34 +0300

    Добавлены проверки для распознавания страницы

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,8 +1,6 @@
-  <!-- footer -->
-  <script src="js/swiper.js"></script>
-  <script src="js/jquery.js"></script>
-  <script src="js/main.js"></script>
-  </body>
+<script src="js/swiper.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script>
+</body>
 
-  </html>
-  <!-- footer-END -->
+</html>
--- blocks/header_page.php
+++ blocks/header_page.php
@@ -1,4 +1,78 @@
-<!-- header -->
+<?php
+$products = [
+  'filters' => [
+    'all',
+    'table',
+    'sitting'
+  ],
+  'product' => [
+    [
+      'filter' => 'sitting',
+      'name' => 'prowling lounge chair <br> olive',
+      'href' => 'product-armchair-olive.html',
+      'srcset' => 'img/1-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/1-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'sitting',
+      'name' => 'prowling lounge chair <br> grape',
+      'href' => 'product-armchair-grape.html',
+      'srcset' => 'img/2-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/2-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'sitting',
+      'name' => 'prowling lounge chair <br> mushroom',
+      'href' => 'product-armchair-mushroom.html',
+      'srcset' => 'img/3-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/3-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'sitting',
+      'name' => 'lurking pouf <br> olive',
+      'href' => 'product-puff-olive.html',
+      'srcset' => 'img/4-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/4-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'sitting',
+      'name' => 'lurking pouf <br> grape',
+      'href' => 'product-puff-grape.html',
+      'srcset' => 'img/5-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/5-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'sitting',
+      'name' => 'lurking pouf <br> mushroom',
+      'href' => 'product-puff-mushroom.html',
+      'srcset' => 'img/6-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/6-all-product.png',
+      'alt' => 'sitting'
+    ],
+    [
+      'filter' => 'table',
+      'name' => 'liquid table',
+      'href' => 'product-page.php',
+      'srcset' => 'img/7-all-product.webp',
+      'type' => 'image/webp',
+      'src' => 'img/7-all-product.png',
+      'alt' => 'table'
+    ],
+  ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,13 +87,36 @@
 </head>
 
 <body>
-  <header class="header-all-product">
+  <header class="header-all-product 
+  <?php if ($_SERVER['REQUEST_URI'] == '/product-page.php') {
+    echo 'header-product-preview';
+  } ?> ">
     <div class="container">
       <div class="row row-jcsb row-ai-center">
         <div class="wrapper-img wrapper-header-product-page-logo">
           <a href="/"><img src="img/mini-logo.svg" alt=""></a>
         </div>
-        <span class="header__open-close header-all-product__nav-open"></span>
+        <?php if ($_SERVER['REQUEST_URI'] == '/all-product.php') { ?>
+          <nav class="catalog">
+            <ul class="catalog__ul">
+              <?php for ($i = 0, $count = count($products['filters']); $i < $count; $i++) : ?>
+                <li class="catalog__li" data-filter="<? echo $products['filters'][$i] ?>"><? echo $products['filters'][$i] ?></li>
+              <?php endfor ?>
+            </ul>
+            <span class="header__open-close header-all-product__nav-open"></span>
+          </nav>
+        <?php } elseif ($_SERVER['REQUEST_URI'] == '/product-page.php') { ?>
+
+          <nav class="catalog">
+            <ul class="catalog__ul">
+              <?php for ($i = 0, $count = count($products['filters']); $i < $count; $i++) : ?>
+                <li class="catalog__li" data-filter="<? echo $products['filters'][$i] ?>"><a href="all-product.php"><? echo $products['filters'][$i] ?></a></li>
+              <?php endfor ?>
+            </ul>
+            <span class="header__open-close header-all-product__nav-open"></span>
+          </nav>
+
+        <?php } else echo '<span class="header__open-close header-all-product__nav-open"></span>' ?>
       </div>
     </div>
     <!-- block navigation -->
